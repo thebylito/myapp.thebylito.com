980fbba1a36927b6f1719809931a6a02449ea3a1
Welington da Silva <thebylito@gmail.com>
2019-02-26 13:22:03 -0400

    eslint config

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -4,22 +4,22 @@
 const User = use('App/Models/User')
 
 class UserController {
-  async create ({ request }){
-    const data = request.only(['username','email', 'password'])
+  async create ({ request }) {
+    const data = request.only(['username', 'email', 'password'])
     const user = await User.create(data)
     return user
   }
-  async update({ request }) {
+  async update ({ request }) {
     const data = request.only(['username', 'email', 'password', 'id'])
-    const user = await User.find(data.id);
-    user.merge(data);
+    const user = await User.find(data.id)
+    user.merge(data)
     await user.save()
     return user
   }
 
-  async delete({ params, response }){
+  async delete ({ params, response }) {
     const { id } = params
-    const user = await User.find(id);
+    const user = await User.find(id)
     await user.delete()
     response.status(200).send()
   }
